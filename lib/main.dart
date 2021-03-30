04073deb0e60de3f4b26c9a80b8480f2e4bff537
Jordilob <jordi.bravo.bad@gmail.com>
2021-03-30 17:09:39 -0300

    Home Page setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:ptuformulas/src/pages/home_page.dart';
 
 void main() => runApp(MyApp());
 
@@ -6,17 +7,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Material App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Material App Bar'),
-        ),
-        body: Center(
-          child: Container(
-            child: Text('Hello World'),
-          ),
-        ),
-      ),
+      title: 'PTU Formulas',
+      initialRoute: 'home',
+      routes: {
+        'home': (BuildContext context) => HomePage(),
+      },
     );
   }
 }
