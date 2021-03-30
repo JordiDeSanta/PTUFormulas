b38e122095685070d452a1aa5f58a677f0ffd054
Jordilob <jordi.bravo.bad@gmail.com>
2021-03-30 19:27:22 -0300

    Text Styles implementation

---
+++ lib/src/themes/text_theme.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class TextStyles {
+  final TextStyle title =
+      TextStyle(fontSize: 25.0, fontWeight: FontWeight.w300);
+
+  final TextStyle course =
+      TextStyle(fontSize: 30.0, fontWeight: FontWeight.w100);
+}
