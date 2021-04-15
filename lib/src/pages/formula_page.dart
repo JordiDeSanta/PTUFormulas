7cdc9fe53d6d200229d73828b699dc1465736139
Jordilob <jordi.bravo.bad@gmail.com>
2021-04-15 18:05:55 -0400

    Showing params in the formula page

--- lib/src/pages/formula_page.dart
+++ lib/src/pages/formula_page.dart
@@ -1,12 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:ptuformulas/src/providers/_provider.dart';
+import 'package:ptuformulas/src/themes/text_theme.dart';
 
 class FormulaPage extends StatelessWidget {
-  const FormulaPage({Key key}) : super(key: key);
+  FormulaPage();
+
+  final styles = TextStyles();
 
   @override
   Widget build(BuildContext context) {
+    final FormulaButtonArguments args =
+        ModalRoute.of(context).settings.arguments;
+
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        title: Text('Formula', style: styles.title),
+        centerTitle: true,
+        toolbarHeight: 80.0,
+        backgroundColor: Colors.black12,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(20.0),
+        ),
+      ),
+      body: Column(
+        children: _createParams(args),
+      ),
     );
+  }
+
+  List<Widget> _createParams(FormulaButtonArguments args) {
+    List<Widget> _params = [SizedBox(height: 10.0)];
+
+    args.params.forEach(
+      (key, value) {
+        Widget _temp = Text(key);
+        _params.add(_temp);
+      },
+    );
+
+    return _params;
   }
 }
