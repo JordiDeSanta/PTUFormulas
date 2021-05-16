293b6034cc69f4b426ea6242bcd9ad885853fdae
Jordi <jordi.bravo.bad@gmail.com>
2021-05-15 22:50:27 -0400

    Triangle Refactor

--- lib/src/widgets/params_triangle.dart
+++ lib/src/widgets/params_triangle.dart
@@ -22,79 +22,74 @@ class ParamsTriangle extends StatelessWidget {
 
     return Stack(
       children: [
-        Icon(
-          CupertinoIcons.triangle_fill,
-          size: width * 1.1,
-          color: args.triangleColor,
-        ),
-        Positioned(
-          width: width * 0.5,
-          height: height * 0.1,
-          left: width * 0.20,
-          top: height * 0.25,
-          child: Text(
-            paramsList[0] != 0.0
-                ? (paramsList[0] * multList[0].selectedMed)
-                        .toStringAsPrecision(3) +
-                    ' ' +
-                    args.resultsSystem[0]
-                : args.formula(args.params).toStringAsPrecision(3) +
-                    ' ' +
-                    args.resultsSystem[0],
-            style: styles.getTriangle(context),
-            textAlign: TextAlign.center,
+        Container(
+          alignment: Alignment.center,
+          child: Icon(
+            CupertinoIcons.triangle_fill,
+            color: args.triangleColor,
+            size: width * 1.1,
           ),
         ),
-        Positioned(
-          width: width * 0.5,
-          height: height * 0.1,
-          left: width * 0.02,
-          top: height * 0.6,
-          child: Text(
-            paramsList[1] != 0
-                ? (paramsList[1] * multList[1].selectedMed)
-                        .toStringAsPrecision(3) +
-                    ' ' +
-                    args.resultsSystem[1]
-                : args.formula(args.params).toStringAsPrecision(3) +
-                    ' ' +
-                    args.resultsSystem[1],
-            style: styles.getTriangle(context),
-            textAlign: TextAlign.center,
-          ),
+        Column(
+          children: [
+            SizedBox(height: width * height * 0.0004),
+            Text(
+              paramsList[0] != 0.0
+                  ? (paramsList[0] * multList[0].selectedMed)
+                          .toStringAsPrecision(3) +
+                      ' ' +
+                      args.resultsSystem[0]
+                  : args.formula(args.params).toStringAsPrecision(3) +
+                      ' ' +
+                      args.resultsSystem[0],
+              style: styles.getTriangle(context),
+              textAlign: TextAlign.center,
+            ),
+            SizedBox(height: height * 0.06),
+            Container(
+              width: width * 0.5,
+              child: Text(
+                'Define como 0 el valor que quieras calcular',
+                style: styles
+                    .getResult(context)
+                    .copyWith(color: Colors.white, fontSize: height * 0.03),
+                overflow: TextOverflow.visible,
+                textAlign: TextAlign.center,
+              ),
+            ),
+            SizedBox(height: height * 0.07),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  paramsList[1] != 0
+                      ? (paramsList[1] * multList[1].selectedMed)
+                              .toStringAsPrecision(3) +
+                          ' ' +
+                          args.resultsSystem[1]
+                      : args.formula(args.params).toStringAsPrecision(3) +
+                          ' ' +
+                          args.resultsSystem[1],
+                  style: styles.getTriangle(context),
+                  textAlign: TextAlign.center,
+                ),
+                SizedBox(width: width * 0.2),
+                Text(
+                  paramsList[2] != 0
+                      ? (paramsList[2] * multList[2].selectedMed)
+                              .toStringAsPrecision(3) +
+                          ' ' +
+                          args.resultsSystem[2]
+                      : args.formula(args.params).toStringAsPrecision(3) +
+                          ' ' +
+                          args.resultsSystem[2],
+                  style: styles.getTriangle(context),
+                  textAlign: TextAlign.center,
+                ),
+              ],
+            ),
+          ],
         ),
-        Positioned(
-          width: width * 0.5,
-          height: height * 0.1,
-          right: width * 0.02,
-          top: height * 0.6,
-          child: Text(
-            paramsList[2] != 0
-                ? (paramsList[2] * multList[2].selectedMed)
-                        .toStringAsPrecision(3) +
-                    ' ' +
-                    args.resultsSystem[2]
-                : args.formula(args.params).toStringAsPrecision(3) +
-                    ' ' +
-                    args.resultsSystem[2],
-            style: styles.getTriangle(context),
-            textAlign: TextAlign.center,
-          ),
-        ),
-        Positioned(
-          height: height * 0.2,
-          width: width * 0.6,
-          left: width * 0.2,
-          top: height * 0.42,
-          child: Text(
-            'Define como 0 el valor que quieras calcular',
-            style: styles
-                .getResult(context)
-                .copyWith(color: Colors.white, fontSize: height * 0.03),
-            overflow: TextOverflow.visible,
-            textAlign: TextAlign.center,
-          ),
-        )
       ],
     );
   }
